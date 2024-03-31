425ce37214afa334a6cf000c4fcb47ba27a52a61
DindaAyuAprilia <126172409+DindaAyuAprilia@users.noreply.github.com>
2024-03-31 09:39:48 +0800

    Add files via upload

---
+++ POSTTEST4/ErrorHandling.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ErrorHandling {
+    // Deklarasi variabel utama 
+
+    // Deklarasi objek yang akan digunakan
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static Tampilan look = new Tampilan();
+
+
+    public int harusAngka(String kalimat){
+        while (true) {
+            try {
+                System.out.print(kalimat);
+                int angka = Integer.parseInt(input.readLine());
+                return angka;
+            } 
+            
+            // Terdapat huruf pada inputan
+            catch (NumberFormatException | IOException e) {
+                look.notif("Hanya menerima inputan angka, silahkan coba lagi...");
+            }
+        }
+    }
+
+    public String harusAngkaString(String kalimat){
+        int angka = harusAngka(kalimat);
+        String angkaString = String.valueOf(angka);
+        return angkaString;    
+    }
+
+    public int batasan(int min, int max, String kalimat){
+        while(true){
+            int angka = harusAngka(kalimat);
+
+            // Cek apakah angka memenuhi batasan
+            if (angka < min){
+                look.notif("Angka yang di masukan terlalu kecil...");
+            }
+
+            else if (angka > max){
+                look.notif("Angka yang di masukan terlalu besar...");
+            }
+
+            else{
+                return angka;
+            }
+        }
+    }
+
+    public String harusHuruf(String kalimat) {
+        while (true) {
+            try {
+                System.out.print(kalimat);
+                String huruf = input.readLine();
+
+                // Memastikan hanya huruf yang dimasukkan
+                if (huruf.matches("[a-zA-Z]+")) { 
+                    return huruf;
+                } 
+                
+                else {
+                    look.notif("Hanya menerima inputan huruf, silahkan coba lagi...");
+                }
+            } 
+            
+            catch (IOException e) {
+                look.notif("Terjadi kesalahan saat membaca input, silahkan coba lagi...");
+            }
+        }
+    }
+}
